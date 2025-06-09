c764ef7260011e623833fa55c37a2c732a8d9924
stackgen-automation <automation@stackgen.com>
2025-06-09 07:49:52 +0000

    Automated commit

---
+++ terraform/README.md
@@ -0,0 +1,3 @@
+# README
+This is a readme file for IaC generated with StackGen.
+You can modify your appStack -> [here](http://main.dev.stackgen.com/appstacks/ba186f3a-2e54-42df-97b7-e0dbb47d8f60)
---
+++ terraform/outputs.tf
@@ -0,0 +1,20 @@
+output "aws_s3_stackgen_c2203638-5a67-46c1-a588-6bb445347936_arn" {
+  value     = module.stackgen_c2203638-5a67-46c1-a588-6bb445347936.arn
+  sensitive = false
+}
+
+output "aws_s3_stackgen_c2203638-5a67-46c1-a588-6bb445347936_bucket_name" {
+  value     = module.stackgen_c2203638-5a67-46c1-a588-6bb445347936.bucket_name
+  sensitive = false
+}
+
+output "aws_s3_stackgen_c2203638-5a67-46c1-a588-6bb445347936_bucket_website_endpoint" {
+  value     = module.stackgen_c2203638-5a67-46c1-a588-6bb445347936.bucket_website_endpoint
+  sensitive = false
+}
+
+output "aws_s3_stackgen_c2203638-5a67-46c1-a588-6bb445347936_kms_arn" {
+  value     = module.stackgen_c2203638-5a67-46c1-a588-6bb445347936.kms_arn
+  sensitive = false
+}
+
