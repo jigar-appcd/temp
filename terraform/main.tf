b0e8c0df5f0b15a9661580e924447100915b15db
stackgen-automation <automation@stackgen.com>
2025-06-18 16:49:28 +0000

    Automated commit

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+module "stackgen_7431ee66-28ec-4025-af91-0964595deec2" {
+  source                       = "./modules/aws_s3"
+  block_public_access          = true
+  bucket_name                  = var.bucket_name
+  bucket_policy                = ""
+  enable_versioning            = true
+  enable_website_configuration = false
+  sse_algorithm                = "aws:kms"
+  tags                         = {}
+  website_error_document       = "404.html"
+  website_index_document       = "index.html"
+}
+
---
+++ terraform/outputs.tf
@@ -0,0 +1,20 @@
+output "aws_s3_stackgen_7431ee66-28ec-4025-af91-0964595deec2_arn" {
+  value     = module.stackgen_7431ee66-28ec-4025-af91-0964595deec2.arn
+  sensitive = false
+}
+
+output "aws_s3_stackgen_7431ee66-28ec-4025-af91-0964595deec2_bucket_name" {
+  value     = module.stackgen_7431ee66-28ec-4025-af91-0964595deec2.bucket_name
+  sensitive = false
+}
+
+output "aws_s3_stackgen_7431ee66-28ec-4025-af91-0964595deec2_bucket_website_endpoint" {
+  value     = module.stackgen_7431ee66-28ec-4025-af91-0964595deec2.bucket_website_endpoint
+  sensitive = false
+}
+
+output "aws_s3_stackgen_7431ee66-28ec-4025-af91-0964595deec2_kms_arn" {
+  value     = module.stackgen_7431ee66-28ec-4025-af91-0964595deec2.kms_arn
+  sensitive = false
+}
+
