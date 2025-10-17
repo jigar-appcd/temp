bb24b63d53b9508e8e605f070e7273d915cca7bf
stackgen-automation <automation@stackgen.com>
2025-10-17 14:28:53 +0000

    Commit by Jigar Moradiya

---
+++ tf/main.tf
@@ -0,0 +1,13 @@
+module "stackgen_6fd6c2a1-7349-4a0c-90ef-6cdc83d0258e" {
+  source                       = "./modules/aws_s3"
+  block_public_access          = true
+  bucket_name                  = "test"
+  bucket_policy                = ""
+  enable_versioning            = true
+  enable_website_configuration = false
+  sse_algorithm                = "aws:kms"
+  tags                         = {}
+  website_error_document       = "404.html"
+  website_index_document       = "index.html"
+}
+
---
+++ tf/outputs.tf
@@ -0,0 +1,20 @@
+output "aws_s3_stackgen_6fd6c2a1-7349-4a0c-90ef-6cdc83d0258e_arn" {
+  value     = module.stackgen_6fd6c2a1-7349-4a0c-90ef-6cdc83d0258e.arn
+  sensitive = false
+}
+
+output "aws_s3_stackgen_6fd6c2a1-7349-4a0c-90ef-6cdc83d0258e_bucket_name" {
+  value     = module.stackgen_6fd6c2a1-7349-4a0c-90ef-6cdc83d0258e.bucket_name
+  sensitive = false
+}
+
+output "aws_s3_stackgen_6fd6c2a1-7349-4a0c-90ef-6cdc83d0258e_bucket_website_endpoint" {
+  value     = module.stackgen_6fd6c2a1-7349-4a0c-90ef-6cdc83d0258e.bucket_website_endpoint
+  sensitive = false
+}
+
+output "aws_s3_stackgen_6fd6c2a1-7349-4a0c-90ef-6cdc83d0258e_kms_arn" {
+  value     = module.stackgen_6fd6c2a1-7349-4a0c-90ef-6cdc83d0258e.kms_arn
+  sensitive = false
+}
+
